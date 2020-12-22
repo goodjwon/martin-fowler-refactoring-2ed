930938746fd243e7f534e0864f1fc64ffe3c0e29
JungWon Park <firstman9@protonmail.com>
2020-12-22 14:23:17 +0900

    format => format 함수로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,18 +3,11 @@ export {statement}
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
+
     let result = `청구내역: (고객명: ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFraction: "Digits: 2"
-        }).format;
 
     for (let perf of invoice.performances) {
-        //포인트를 적입한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits +=volumeCreatesFor(perf)
 
         // 청구내역을 출력한다.
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} 석)\n`;
@@ -25,6 +18,22 @@ function statement(invoice, plays) {
     result += `적립포인트: ${volumeCredits}점\n`;
 
     return result;
+
+    function format(aNumber){
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFraction: "Digits: 2"
+            }).format(aNumber)
+    }
+
+    function volumeCreatesFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+
+        return volumeCredits;
+    }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
