850ee7c4b408e8dd863371426e335736ffb7995f
JungWon Park <firstman9@protonmail.com>
2020-12-22 14:51:26 +0900

    volumeCredits => totalVolumeCredits() 함수로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -2,20 +2,26 @@ export {statement}
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
+
     let result = `청구내역: (고객명: ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        volumeCredits +=volumeCreatesFor(perf)
-
         // 청구내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} 석)\n`;
         totalAmount += amountFor(perf);
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립포인트: ${volumeCredits}점\n`;
+    result += `적립포인트: ${totalVolumeCredits()}점\n`;
 
     return result;
+
+    function totalVolumeCredits(){
+        let volumeCredits = 0;
+        for(let perf of invoice.performances){
+            volumeCredits +=volumeCreatesFor(perf)
+        }
+        return volumeCredits;
+    }
 
     function usd(aNumber){
         return new Intl.NumberFormat("en-US",
